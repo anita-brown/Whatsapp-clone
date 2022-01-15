070283f245d0524417b1ce233a912263458256c1
kaydayo <olukayodedayo100@gmail.com>
2022-01-15 19:20:23 +0100

    created a handler called verifyEmail to handle confirmation email

--- src/controller/verifyEmail.ts
+++ src/controller/verifyEmail.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bcrypt from 'bcryptjs';
 import User from '../model/mockUser.model';
 // import jwt, { Secret } from 'jsonwebtoken'
@@ -6,5 +6,20 @@ import User from '../model/mockUser.model';
 
 
 export const verifyEmail = async (req: Request, res: Response, next: NextFunction) => {
-    const user = await User.findOne({})
+    try {
+        const user = await User.findOne({ confirmationCode: req.params.confirmationCode })
+        if (!user) {
+            return res.status(404).json({ message: 'user not found' })
+        }
+        user.status = "Active";
+        user.save((err: any) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+        })
+    } catch (err: any) {
+        console.log("error", err)
+    }
 }
+
