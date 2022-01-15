e843140360ca03dde3764da5ea233a87346575ce
kaydayo <olukayodedayo100@gmail.com>
2022-01-15 23:46:02 +0100

    created login handler to enable user login, after user has verified email they are redirected to a login route

--- src/controller/verifyEmail.ts
+++ src/controller/verifyEmail.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
-import bcrypt from 'bcryptjs';
 import User from '../model/mockUser.model';
-// import jwt, { Secret } from 'jsonwebtoken'
+import { validateLogin } from "../utils/validatorUtils";
+import bcrypt from 'bcryptjs'
+import jwt, { Secret } from 'jsonwebtoken'
 
 
 
@@ -25,11 +26,22 @@ export const verifyEmail = async (req: Request, res: Response, next: NextFunctio
 }
 
 export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-    const {error} = validateLogin(req.body)
-    try {
-        
-    } catch (error: any) {
-        
+    const { error } = validateLogin(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    } else {
+        try {
+            const findUser = await User.findOne({ email: req.body.email })
+            if (!findUser) return res.status(400).json({ message: 'invalid login credentials' })
+            const match = await bcrypt.compare(req.body.password, findUser.password)
+            if (!match) return res.status(400).json({ message: 'invalid login credentials' })
+            if(findUser.status === 'Pending') return res.status(403).json({
+                message: `kindly verify your account via the email sent to you`})
+            const user = {email: req.params.email}
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as Secret)
+            res.status(201).json({ message: 'login successful', accessToken })
+        } catch (error: any) {
+
+        }
     }
 }
-
