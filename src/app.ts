b58ed870dd22759b55c3e56feab9d33a71c03d28
OmoladeAkingbade <lademercy93@gmail.com>
2022-01-18 23:57:38 +0100

    added google-login route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import './auth';
+import passport from 'passport';
+import session from 'express-session';
 import { mongoDBConnect, mongoMockConnect } from './database/database';
 
-const app: Application = express();
+dotenv.config();
 
+const app: Application = express();
 // Express body parser
 app.use(express.json());
+
+// Google-auth
+const googleLogin = (req: Request, res: Response, next: NextFunction) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+}
+
+const protectedR = (passport.authenticate('google', { scope: ['email', 'profile'] }))
+
+app.get('/', googleLogin)
+app.get('/auth/google', protectedR)
 
 // To log our requests using morgan, but only during developement
 if ((process.env.NODE_ENV = 'development')) {
