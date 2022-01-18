b7fb001b113a8e0538f5f6728b7c8c3a8e4d1c1b
OmoladeAkingbade <lademercy93@gmail.com>
2022-01-19 00:06:42 +0100

    implemented google strategy

---
+++ src/auth.ts
@@ -0,0 +1,32 @@
+import passport from "passport";
+import express,{Request, Response, NextFunction  } from "express";
+
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: process.env.CALLBACK_URL as string,
+      passReqToCallback: true,
+    },
+    function (
+      request: Request,
+      accessToken: any,
+      refreshToken: any,
+      profile: any,
+      cb: Function
+    ) {
+      return cb(null, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  return done(null, user);
+});
+
+passport.deserializeUser(function (user: Express.User, done) {
+  return done(null, user);
+});
