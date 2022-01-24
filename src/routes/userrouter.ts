1034b34ec327be82609795564590b459b9996215
chei7tain <fredrickokwuobi@gmail.com>
2022-01-24 21:59:36 +0100

    updated userschema

--- src/routes/userrouter.ts
+++ src/routes/userrouter.ts
@@ -1,8 +1,18 @@
 import { Router, Request, Response, NextFunction } from 'express';
 
 const router = Router();
+const User = require('../models/userSchema');
 
-router.route('/').get((req: Request, res: Response) => {
-  res.send('Hello world');
+router.route('/').get(async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      message: 'Success',
+      dataLength: users.length,
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 module.exports = router;
