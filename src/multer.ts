b187360873a7e5585474c935aa9b32c2cd20db3c
chei7tain <fredrickokwuobi@gmail.com>
2022-01-27 15:28:30 +0100

    add ulter config

---
+++ src/multer.ts
@@ -0,0 +1,44 @@
+// const multer = require('multer');
+// const cloudinary = require('cloudinary').v2;
+// // define the storage for the images
+// const storage = multer.diskStorage({
+//   // destination for the files
+//   destination: function (
+//     req: Request,
+//     file: any,
+//     cb: (error: Error | null, destination: string) => void
+//   ) {
+//     cb(null, './public/uploads/images');
+//   },
+//   // filename for the files
+//   filename: function (
+//     req: Request,
+//     file: any,
+//     cb: (error: Error | null, fileName: string) => void
+//   ) {
+//     cb(null, Date.now() + file.originalname);
+//   },
+// });
+// // upload parameters for multer
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1024 * 1024 * 5,
+//   },
+// });
+
+const multer = require("multer");
+const path = require("path");
+
+// multer config
+module.exports = multer({
+  storage:multer.diskStorage({}),
+  // filter file just incase users try to upload other files
+  fileFilter:(req:Request,file:any,cb: (arg0: Error | null, arg1: boolean) => void)=>{
+    let ext = path.extname(file.originalname);
+    if(ext !==".jpg" && ext !==".png" && ext !==".jpeg"){
+      return cb(new Error("Only images are allowed"),false);
+    }
+    cb(null,true);
+  }
+})
