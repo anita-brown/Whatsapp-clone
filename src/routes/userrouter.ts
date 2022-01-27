8024c57aebdda93a2d3515ba6ddffae864c08ec6
chei7tain <fredrickokwuobi@gmail.com>
2022-01-27 15:14:22 +0100

    add types for multer

--- src/routes/userrouter.ts
+++ src/routes/userrouter.ts
@@ -1,8 +1,29 @@
 import { Router, Request, Response, NextFunction } from 'express';
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 
 const router = Router();
 const User = require('../models/userSchema');
 
+// define the storage for the images
+const storage = multer.diskStorage({
+  // destination for the files
+  destination:function(req:Request,file:any,cb:(error:Error|null,destination:string)=>void){
+    cb(null,'./public/uploads/images');
+  },
+  // filename for the files
+  filename:function(req:Request,file:any,cb:(error:Error|null,fileName:string)=>void){
+    cb(null,Date.now() + file.originalname);
+  },
+
+})
+// upload parameters for multer
+const upload = multer({
+  storage:storage,
+  limits:{
+    fileSize:1024 * 1024 * 5
+  }
+})
 router.route('/').get(async (req: Request, res: Response) => {
   try {
     const users = await User.find();
