cdc2ec086edf37dfce6862076e76c32a28030701
chei7tain <fredrickokwuobi@gmail.com>
2022-01-29 01:03:52 +0100

    facebook sso tested and works however requires a secure https protocol in order to make the creation

--- dist/passport/passport-FB-Setup.js
+++ dist/passport/passport-FB-Setup.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,14 +16,14 @@ exports.setupFB = void 0;
 const passport_1 = __importDefault(require("passport"));
 const passport_facebook_1 = __importDefault(require("passport-facebook"));
 // import { UserAuth as UserFB } from '../models/Users';
-const userModel_1 = require("../models/userModel");
+const Users_1 = require("../models/Users");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 passport_1.default.serializeUser((user, done) => {
     done(null, user);
 });
 passport_1.default.deserializeUser((id, done) => {
-    userModel_1.UserFr.findById(id).then((user) => {
+    Users_1.UserAuth.findById(id).then((user) => {
         done(null, user);
     });
 });
@@ -26,22 +35,13 @@ const setupFB = () => {
         callbackURL: 'http://localhost:3050/api/v1/users/facebook/redirect',
         profileFields: ['id', 'emails'],
     }, function (accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        // Check if user already Exist
-        userModel_1.UserFr.findOne({ facebookId: profile.id })
-            .then((currentUser) => {
-            if (currentUser) {
-                // Already have user
-                // console.log(`User is, ${currentUser}`);
-                return done(null, currentUser);
-            }
-            else {
-                // Create User
-                const details = {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const user = {
                     fullname: profile.displayName,
                     facebookId: profile.id,
                 };
-                // const details = {
+                // const user = {
                 //   firstName: profile.displayName,
                 //   lastName: profile.displayName,
                 //   email: profile._json.email,
@@ -51,19 +51,18 @@ const setupFB = () => {
                 //   password: 'FACEBOOK',
                 //   // phoneNumber: profile
                 // };
-                new userModel_1.UserFr(details)
-                    .save()
-                    .then((newUser) => {
-                    // console.log(`New User Created ${newUser}`);
-                    return done(null, newUser);
-                })
-                    .catch((err) => {
-                    console.log(err);
-                });
+                const currentUser = (yield Users_1.UserAuth.findOne({
+                    facebookId: profile.id,
+                })) || (yield Users_1.UserAuth.findOne({ email: profile._json.email }));
+                if (currentUser) {
+                    return done(null, currentUser);
+                }
+                const newUser = yield Users_1.UserAuth.create(user);
+                return done(null, newUser);
             }
-        })
-            .catch((err) => {
-            console.log(err);
+            catch (error) {
+                console.log(error);
+            }
         });
     }));
 };
--- src/passport/passport-FB-Setup.ts
+++ src/passport/passport-FB-Setup.ts
@@ -1,14 +1,14 @@
 import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
 // import { UserAuth as UserFB } from '../models/Users';
-import { UserFr as UserFB } from '../models/userModel';
+import { UserAuth as Users } from '../models/Users';
 import dotenv from 'dotenv';
 dotenv.config();
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 passport.deserializeUser((id, done) => {
-  UserFB.findById(id).then((user) => {
+  Users.findById(id).then((user) => {
     done(null, user);
   });
 });
@@ -22,47 +22,38 @@ export const setupFB = () => {
         callbackURL: 'http://localhost:3050/api/v1/users/facebook/redirect',
         profileFields: ['id', 'emails'],
       },
-      function (accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        // Check if user already Exist
-        UserFB.findOne({ facebookId: profile.id })
-          .then((currentUser) => {
-            if (currentUser) {
-              // Already have user
-              // console.log(`User is, ${currentUser}`);
-              return done(null, currentUser);
-            } else {
-              // Create User
-              const details = {
-                fullname: profile.displayName,
-                facebookId: profile.id,
-              };
-              // const details = {
-              //   firstName: profile.displayName,
-              //   lastName: profile.displayName,
-              //   email: profile._json.email,
-              //   facebookId: profile.id,
-              //   isVerified: true,
-              //   status: 'Active',
-              //   password: 'FACEBOOK',
-              //   // phoneNumber: profile
-              // };
-              new UserFB(details)
-                .save()
-                .then((newUser: any) => {
-                  // console.log(`New User Created ${newUser}`);
-                  return done(null, newUser);
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+      async function (accessToken, refreshToken, profile, done) {
+        try {
+          const user = {
+            fullname: profile.displayName,
+            facebookId: profile.id,
+          };
+          // const user = {
+          //   firstName: profile.displayName,
+          //   lastName: profile.displayName,
+          //   email: profile._json.email,
+          //   facebookId: profile.id,
+          //   isVerified: true,
+          //   status: 'Active',
+          //   password: 'FACEBOOK',
+          //   // phoneNumber: profile
+          // };
+
+          const currentUser =
+            (await Users.findOne({
+              facebookId: profile.id!,
+            })) || (await Users.findOne({ email: profile._json.email! }));
+          if (currentUser) {
+            return done(null, currentUser);
+          }
+          const newUser = await Users.create(user);
+          return done(null, newUser);
+        } catch (error) {
+          console.log(error);
+        }
       }
     )
   );
 };
+
 export default setupFB;
